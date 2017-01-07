c899d5b286f3df4e2678130fc61e4cd04516b81f
greenchuckaa@gmail.com <greenchuckaa@gmail.com>
2017-01-07 20:58:11 +0200

    commite view

---
+++ public/dopForma/app/app.js
@@ -0,0 +1,34 @@
+Ext.application({
+    name: 'dopForma',
+    appFolder: 'app',
+    controllers: [
+           // 'dicWizardController'
+    ],
+    launch:function(){
+           alert('dddd');
+            /*
+    Ext.create('Ext.window.Window', {
+    title: 'Довідники',
+    height: 300,
+    width: 300,
+    
+    items: [
+           {
+             html : 'ffff'     
+           },
+           {
+             html : 'ffff'   
+           }
+           
+    ]
+    }).show();         
+            */
+            
+            
+    
+   
+           
+    }
+    
+});
+
